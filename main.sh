aa17299c5474bdb4dc6c6e9d48fa1e6934437c88
myzhero <148290721+myzhero@users.noreply.github.com>
2023-11-17 10:05:56 +0700

    Add files via upload

--- main.sh
+++ main.sh
@@ -597,7 +597,7 @@ apt-get install dropbear -y > /dev/null 2>&1
 wget -q -O /etc/default/dropbear "${REPO}files/dropbear.conf"
 chmod +x /etc/default/dropbear
 /etc/init.d/dropbear restart
-/etc/init.d/dropbear status
+systemctl restart dropbear
 print_success "Dropbear"
 }
 
