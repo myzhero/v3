622fa3ac64d72069b364612f6f85cb849c9c0ed2
myzhero <148290721+myzhero@users.noreply.github.com>
2023-11-17 09:58:11 +0700

    Add files via upload

--- fix/dropbear.sh
+++ fix/dropbear.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
-
+apt -y remove dropbear
+sleep 0.5
+apt -y purge dropbear
+sleep 0.5
+apt-get -y --purge remove dropbear
+sleep 0.5
 apt-get install dropbear -y > /dev/null 2>&1
-wget -q -O /etc/default/dropbear "${REPO}files/dropbear.conf"
+wget -q -O /etc/default/dropbear "https://raw.githubusercontent.com/myzhero/v3/files/dropbear.conf"
 chmod +x /etc/default/dropbear
 /etc/init.d/dropbear restart
-/etc/init.d/dropbear status
+systemctl restart dropbear
